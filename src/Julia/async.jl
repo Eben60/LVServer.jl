4e954b40358b599bae97f83d6e74532c64236757
Eben60 <8xe8n-cm@online.de>
2022-01-25 12:06:01 +0100

    untested

--- src/Julia/async.jl
+++ src/Julia/async.jl
@@ -1,33 +1,34 @@
 import Base.Threads.@spawn
 
+# untested
+mutable struct Tasks
+    d::Dict{Int, Any}
+    c::Int # counter
+end
+
 const tasks = Dict{Int, Any}()
 
-function getticket()
-    global tasks
-    if isempty(tasks)
-        return 1
-    else
-        return maximum(keys(tasks)) +1
-    end
+function getticket(tasks)
+    return tasks.c += 1
 end
 
 function asyncall(f; kwargs...)
     global tasks
-    ticket = getticket()
+    ticket = getticket(tasks)
     id = @spawn f(; kwargs...)
-    push!(tasks, ticket=>id)
+    push!(tasks.d, ticket=>id)
     return (;ticket)
 end
 
 function checkjob(; ticket)
     global tasks
-    id = tasks[ticket]
+    id = tasks.d[ticket]
     done = istaskdone(id)
     if !done
         return (; done)
     else
         result = fetch(id)
-        pop!(tasks, ticket)
+        pop!(tasks.d, ticket)
         return (; done, result)
     end
 end
