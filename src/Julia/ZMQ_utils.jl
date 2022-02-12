92f49e5ddaab718ab87231691aefa4f9143c61e0
Eben60 <8xe8n-cm@online.de>
2022-02-12 12:34:45 +0100

    work in progress

--- src/Julia/ZMQ_utils.jl
+++ src/Julia/ZMQ_utils.jl
@@ -257,12 +257,12 @@ function version_this_pkg()
 end
 
 function trunc_v(v)
-    return v # TODO maj.min.patch
+    return VersionNumber(v.major, v.minor, v.patch) # v # TODO maj.min.patch
 end
 
 function check_pkg_versions(;LV_v, LVServer_current, LVServer_min)
-    LabVIEW0_LOCAL_CURRENT = v"0.2.0"
-    LabVIEW0_LOCAL_MIN = v"0.1.0"
+    LabVIEW0_LOCAL_CURRENT = v"0.3.0"
+    LabVIEW0_LOCAL_MIN = v"0.2.0"
 
     LV_v = VersionNumber(NamedTuple(LV_v)...)
     LVServer_min = VersionNumber(NamedTuple(LVServer_min)...)
@@ -278,11 +278,43 @@ function check_pkg_versions(;LV_v, LVServer_current, LVServer_min)
     LVServer_OK = LVServer_remote_current == trunc_v(LVServer_localv)
     LV_OK = trunc_v(LabVIEW0_LOCAL_CURRENT) == LV_v
 
+    if LVServer_incompat
+        err_msg = "The LVServer package version on this Julia server is $LVServer_localv. \
+        At least $LVServer_min is expected by LabVIEW0 client."
+    end
+
+    if LV_incompat
+        err_msg = "The version of LabVIEW0 client (or of currently used LV2Julia_core.lvlib) is $LV_v. \
+        At least $LabVIEW0_LOCAL_MIN is expected by LVServer package version on this Julia server"
+    end
+
+    if (LVServer_incompat | LV_incompat)
+        @warn err_msg # for local display on the server
+        throw(ErrorException(err_msg)) # returned to the LV client
+    end
+
+    if !LVServer_OK
+        warn_msg = "The LVServer package version on this Julia server is $LVServer_localv. \
+        According to LabVIEW0 client, $LVServer_remote_current is available, please consider LVServer update."
+
+    end
+
+    if !LV_OK
+        warn_msg = "The version of LabVIEW0 client (or of currently used LV2Julia_core.lvlib) is $LV_v. \
+        According to LVServer package on this Julia server, $LabVIEW0_LOCAL_CURRENT  is available, \
+        please consider LabVIEW0 update"
+    end
+
+    if !(LVServer_OK & LV_OK)
+        @warn warn_msg # for local display on the server
+    end
+
 
     return nothing
 end
 
-# lvnt = (; LV_v=(major=1, minor=2, patch=3), LVServer_current=(major=4, minor=5, patch=6), LVServer_min=(major=0, minor=3, patch=0));
+# lvnt_err = (; LV_v=(major=0, minor=1, patch=3), LVServer_current=(major=4, minor=5, patch=6), LVServer_min=(major=0, minor=3, patch=0));
+# lvnt = (; LV_v=(major=0, minor=2, patch=1), LVServer_current=(major=4, minor=5, patch=6), LVServer_min=(major=0, minor=2, patch=0));
 # lvjs = JSON3.write(lvnt);
 # lvnt1 = lvnt1=JSON3.read(lvjs);
 
